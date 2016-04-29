df49a71e9a513130812843d4ede726f81eb267cd
Anthony Vatousiou <vatousiou.a@husky.neu.edu>
2016-04-29 14:44:38 -0400

    Changes to displaying the badges tab

--- public/project/server/models/league.schema.server.js
+++ public/project/server/models/league.schema.server.js
@@ -1,13 +1,16 @@
 module.exports = function(mongoose) {
 
+    var GymLeaderSchema = require('./gymleader.schema.server.js')(mongoose);
+    var EliteFourSchema = require('./elitefour.schema.server.js')(mongoose);
+
     var LeagueSchema = mongoose.Schema({
         "leagueName": String,
         "difficulty": {
             type: String,
             enum: ["Easy", "Medium", "Hard"]
         },
-        "gymLeaders": [String],
-        "eliteFour": [String]
+        "gymLeaders": [GymLeaderSchema],
+        "eliteFour": [EliteFourSchema]
     });
 
     return LeagueSchema;
